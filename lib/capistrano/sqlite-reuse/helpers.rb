1a3d6b33e1b43447b0458dbe645666734c9c4259
Mike Crockett <github@mmcrockett.com>
2017-06-06 13:44:10 -0500

    Fixing yaml access with symbol or string.

--- lib/capistrano/sqlite-reuse/helpers.rb
+++ lib/capistrano/sqlite-reuse/helpers.rb
@@ -4,6 +4,7 @@ module Capistrano
   module SqliteReuse
     module Helpers
       def local_database_yml(env = fetch(:rails_env))
+        env = env.to_s
         @local_database_yml ||= YAML.load_file(database_yml_local_path)
 
         return @local_database_yml[env]
@@ -13,22 +14,16 @@ module Capistrano
         return local_database_yml()['database']
       end
 
-      def adatpter_type
+      def adapter_type
         return fetch(:adapter_type)
       end
 
       def check_adapter_error
-        puts
-        puts "Error - not '#{adapter_type}' adpater type in '#{fetch(:database_yml_local_path)}'."
-        puts "May want to remove this gem."
-        puts
+        return "Error - not '#{adapter_type}' adpater type in '#{fetch(:database_yml_local_path)}'.  May want to remove this gem."
       end
 
-      def check_config_present_error
-        puts
-        puts "Error - '#{fetch(:rails_env)}' config not present in '#{fetch(:database_yml_local_path)}'."
-        puts "Please populate it."
-        puts
+      def check_config_exist_error
+        return "Error - '#{fetch(:rails_env)}' config does not exist in '#{fetch(:database_yml_local_path)}'. Please populate it."
       end
     end
   end
